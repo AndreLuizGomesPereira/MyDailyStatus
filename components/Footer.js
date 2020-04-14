550d9e7226394cf03a6c6ef995ce25c2aa9fda12
André Luiz <andregomesenator@gmail.com>
2020-04-14 19:42:24 -0300

    Finalizado primeira parte do projeto com todas as páginas.

---
+++ components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = ()=>{
+    return (
+        <div className='py-4 text-center bg-gray-400'>
+            My Daily Status é um projeto feito pelo treinamento DevPleno.<br />
+            Implementado por:<a href='https://www.linkedin.com/in/andre-luiz-gomes-pereira-509403161/'> André Luiz </a>
+        </div>
+    )
+}
+
+export default Footer
---
+++ components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className='bg-gray-200'>
+            <h1>
+                <img className='h-24 mx-auto py-2' src="/logo.png" alt="Meu Status Diário" />
+            </h1>
+            
+        </div>
+    )
+}
+ export default Header
