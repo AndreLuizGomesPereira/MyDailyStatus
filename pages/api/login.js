be1b1442b4c73210bf19f9f3d6e95ed4a28c5de7
André Luiz <andregomesenator@gmail.com>
2020-04-15 17:18:04 -0300

    Adicionando autorização externa com auth0.

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,10 +1,14 @@
 // request <> response
+import auth0 from '../../lib/auth0'
 
 const login = async(request, response) => {
+await auth0.handleLogin(request, response)
+
     response.send({
         'Nome': 'André Luiz',
         'Idade': '31 anos'
     })
 }
+
 
 export default login
