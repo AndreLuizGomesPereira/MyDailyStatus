8bd841f247d7b1512319d1caffc879d06ce96f97
André Luiz <andregomesenator@gmail.com>
2020-04-15 19:39:52 -0300

    Implementado a parte de login e logout.

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ const Index = () => {
     return (
         <div>
             <h1>Home</h1>
+            <a href='/api/login' className='py-4 px-2 rounded bg-pink-800 font-bold shadow-xl hover:shadow-lg block w-1/4 text-center mx-auto text-white'>Login Aqui</a>
         </div>)
 }
 
