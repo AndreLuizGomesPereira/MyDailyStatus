910aacc1d84df1a4aaf2372eddf8c08c91de9627
André Luiz <andregomesenator@gmail.com>
2020-04-16 17:36:50 -0300

    Projeto iniciado com o banco de dados Firebase.

--- pages/app.js
+++ pages/app.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import auth0 from '../lib/auth0'
+import router from 'next/router'
+import { db } from '../lib/db'
 
 const App = props => {
+    useEffect(() => {
+        if (!props.isAuth) {
+            router.push('/')
+        } else if (props.forceCreate) {
+            router.push('/create-status')
+        }
+    })
+    if (!props.isAuth || props.forceCreate) {
+        return null
+    }
     return (
         <div>
             <h1>App</h1>
@@ -15,14 +27,42 @@ export default App
 export async function getServerSideProps({ req, res }) {
     const session = await auth0.getSession(req)
     if (session) {
+        const today = new Date()
+        const currentDate = today.getDate() + '-' + today.getMonth() + '-' + today.getFullYear()
+        const todaysCheckin = await db
+            .collection('markers')
+            .doc(currentDate)
+            .collection('checks')
+            .doc(session.user.sub)
+            .get()
+        const todaysData = todaysCheckin.data()
+        let forceCreate = true
+        if (todaysData) {
+            // pode ver os outros checkins
+            forceCreate = false
+            const checkins = await db
+                .collection('markers')
+                .doc(currentDate)
+                .collection('checks')
+                .near({
+                    center: todaysData.coordinates,
+                    radius: 1000
+                })
+            checkins.docs.forEach(docs => {
+                console.log(doc.id, doc.data())
+            });
+        }
         return {
             props: {
-                user: session.user
+                isAuth: true,
+                user: session.user,
+                forceCreate
             }
         }
     }
     return {
         props: {
+            isAuth: false,
             user: {}
         }
     }
